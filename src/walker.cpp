c4ac9fdf87e5267d71f8cf3e58b1ea76e1dfe799
VEDANT RANADE <vedantmranade@gmail.com>
2023-11-24 04:07:34 -0500

    Added eMotionState type to store set motionStates of the walker

--- src/walker.cpp
+++ src/walker.cpp
@@ -10,19 +10,30 @@
  */
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
-
+enum class eMotionState{
+  START,
+  FORWARD,
+  TURN,
+  STOP
+};
 class Walker:public rclcpp::Node{
   public:
   Walker():Node("walker"){
     commandVelPublisher_=this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel",10);
     timer_=this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&Walker::timerCallback,this));
+    motionState_=eMotionState::STOP;
   }
   private:
   void timerCallback(){
     ;
   }
+  bool obstacleDetected(){
+    auto obstacleDetected=false;
+    return obstacleDetected;
+  }
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr commandVelPublisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+  eMotionState motionState_;
 };
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
