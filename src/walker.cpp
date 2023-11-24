5164ed49a648ab83178fc4614becfc4c0d8444ca
VEDANT RANADE <vedantmranade@gmail.com>
2023-11-24 04:32:52 -0500

    ran clang format ,added basic loggin

--- src/walker.cpp
+++ src/walker.cpp
@@ -9,6 +9,8 @@
  *
  */
 #include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 enum class eMotionState { START, FORWARD, TURN, DO_NOTHING };
 class Walker : public rclcpp::Node {
@@ -16,13 +18,16 @@ class Walker : public rclcpp::Node {
   Walker() : Node("walker") {
     commandVelPublisher_ =
         this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(10),
                                      std::bind(&Walker::timerCallback, this));
     motionState_ = eMotionState::DO_NOTHING;
   }
 
  private:
-  void timerCallback() { ; }
+  void timerCallback() {
+    RCLCPP_INFO_STREAM(this->get_logger(),"Timer callback called");
+    updateMotionState();
+  }
   void updateMotionState() {
     switch (motionState_) {
       case eMotionState::START:
@@ -52,19 +57,19 @@ class Walker : public rclcpp::Node {
     }
   }
   // @brief: Publishes on cmd_vel topic to move the robot forward in x direction
-  inline void forward(){
+  inline void forward() {
     auto msg = geometry_msgs::msg::Twist();
     msg.linear.x = 0.2;
     commandVelPublisher_->publish(msg);
   }
   // @brief: Publishes on cmd_vel topic to turn the robot in z direction
-  inline void turn(){
+  inline void turn() {
     auto msg = geometry_msgs::msg::Twist();
     msg.angular.z = 0.2;
     commandVelPublisher_->publish(msg);
   }
   // @brief: Publishes on cmd_vel topic to stop the robot
-  inline void stop(){
+  inline void stop() {
     auto msg = geometry_msgs::msg::Twist();
     commandVelPublisher_->publish(msg);
   }
