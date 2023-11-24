d1053ebf30767c077623aec3617d361d0b2a0ae3
VEDANT RANADE <vedantmranade@gmail.com>
2023-11-24 04:02:49 -0500

    Initialized the walker node
    Initialized the basic class definition of walker node
    Initialized the walker executable as a ros node

--- src/walker.cpp
+++ src/walker.cpp
@@ -1,9 +1,28 @@
-#include <cstdio>
+/**
+ * @file walker.cpp
+ * @author Vedant Ranade
+ * @brief ROS2 tbot3 walker node
+ * @version 0.1
+ * @date 2023-11-03
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
 
+class Walker:public rclcpp::Node{
+  public:
+  Walker():Node("walker"){
+
+  }
+  private:
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
 int main(int argc, char** argv) {
-  (void)argc;
-  (void)argv;
-
-  printf("hello world tbot_walker package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Walker>());
+  rclcpp::shutdown();
   return 0;
 }
