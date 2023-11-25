a967ec81616ff190b64163a1618f744305eb3340
VEDANT RANADE <vedantmranade@gmail.com>
2023-11-24 19:29:11 -0500

    Populated launch file

--- launch/walker.py
+++ launch/walker.py
@@ -1,6 +1,23 @@
-import launch
+from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument,ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
-    return launch.LaunchDescription([
+    return LaunchDescription([
+        DeclareLaunchArgument('rosbag_record', default_value = 'true', choices = ['true', 'false'], description = "Enable recording all topics except camera?"),
+        # Launch the publisher node
+        Node(
+            package='tbot_walker',
+            executable='walker',
+            name='walker_node',
+            output='screen'
+        ),
+            ExecuteProcess(
+            condition=IfCondition(LaunchConfiguration('rosbag_record')),
+            cmd=['ros2', 'bag', 'record', '-a'],
+            shell=True
+        )
     ])
